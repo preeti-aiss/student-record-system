4f1351b1b2803e3d3427c069b5518037a86e0d79
preeti-aiss <preeti.cfscsip@gmail.com>
2025-07-30 19:37:07 +0530

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,8 +1,30 @@
 CREATE DATABASE IF NOT EXISTS school;
 USE school;
 
+-- students table
 CREATE TABLE IF NOT EXISTS students (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
-    age INT
+    class VARCHAR(10)
 );
+
+-- marks table
+CREATE TABLE marks (
+    student_id INT,
+    subject VARCHAR(50),
+    marks INT,
+    FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+-- Sample Data
+INSERT INTO students (name, class) VALUES
+('Reyaansh', '6A'),
+('Saanvi', '6A');
+
+INSERT INTO marks (student_id, subject, marks) VALUES
+(1, 'Math', 90),
+(1, 'Science', 88),
+(1, 'English', 85),
+(2, 'Math', 70),
+(2, 'Science', 75),
+(2, 'English', 80);
